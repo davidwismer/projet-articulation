7cace3d8936bc42c6af13ebd4afc62a97221f25b
David Wismer <david.wismer@heig-vd.ch>
2022-06-01 09:13:51 +0200

    test

--- wakanda-horaire/laravel/app/Models/Absence.php
+++ wakanda-horaire/laravel/app/Models/Absence.php
@@ -14,11 +14,17 @@ class Absence extends Model
         'dateFin',
         'motif',
         'isValid',
-        'user_id'
+        'user_id',
+        'cour_id'
     ];
 
     //Definition avec user (une absence vaut pour un seul utilisateur)
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    //Definition avec cours (une absence concerne un seul cours)
+    public function cour(){
+        return $this->belongsTo(Cour::class);
     }
 }
