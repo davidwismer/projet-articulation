a1b56d51b1efb0af2037f10c06458aaa2a3cfc05
Teo Dolci <teo.dolci@heig-vd.ch>
2022-06-09 10:17:09 +0200

    vue.blade.php optimisé

--- wakanda-horaire/laravel/resources/views/vue.blade.php
+++ wakanda-horaire/laravel/resources/views/vue.blade.php
@@ -24,6 +24,51 @@ $tabNotes = [];
 foreach ($datas['notes'] as $note) {
     array_push($tabNotes, $note);
 }
+
+$tabCours = [];
+foreach ($datas['cours'] as $cours) {
+    array_push($tabCours, $cours);
+}
+
+$tabAbsences = [];
+foreach ($datas['absences'] as $absence) {
+    array_push($tabAbsences, $absence);
+}
+
+$tabCommentaires = [];
+foreach ($datas['commentaires'] as $commentaire) {
+    array_push($tabCommentaires, $commentaire);
+}
+
+$tabFilieres = [];
+foreach ($datas['filieres'] as $filiere) {
+    array_push($tabFilieres, $filiere);
+}
+
+$tabGroups = [];
+foreach ($datas['groups'] as $group) {
+    array_push($tabGroups, $group);
+}
+
+$tabRoles = [];
+foreach ($datas['roles'] as $role) {
+    array_push($tabRoles, $role);
+}
+
+$tabFiliereModule = [];
+foreach ($datas['filiere_module'] as $filiereModule) {
+    array_push($tabFiliereModule, $filiereModule);
+}
+
+$tabModuleUser = [];
+foreach ($datas['module_user'] as $moduleUser) {
+    array_push($tabModuleUser, $moduleUser);
+}
+
+$tabBrancheUser = [];
+foreach ($datas['branche_user'] as $brancheUser) {
+    array_push($tabBrancheUser, $brancheUser);
+}
 ?>
 
 
@@ -44,12 +89,20 @@ foreach ($datas['notes'] as $note) {
 <body>
 
     <script>
-        //Evenements
         const tabEvents = <?php echo json_encode($tabEvents); ?>;
         const tabUsers = <?php echo json_encode($tabUsers); ?>;
         const tabBranches = <?php echo json_encode($tabBranches); ?>;
         const tabModules = <?php echo json_encode($tabModules); ?>;
         const tabNotes = <?php echo json_encode($tabNotes); ?>;
+        const tabCours = <?php echo json_encode($tabCours); ?>;
+        const tabAbsences = <?php echo json_encode($tabAbsences); ?>;
+        const tabCommentaires = <?php echo json_encode($tabCommentaires); ?>;
+        const tabFilieres = <?php echo json_encode($tabFilieres); ?>;
+        const tabGroups = <?php echo json_encode($tabGroups); ?>;
+        const tabRoles = <?php echo json_encode($tabRoles); ?>;
+        const tabFiliereModule = <?php echo json_encode($tabFiliereModule); ?>;
+        const tabModuleUser = <?php echo json_encode($tabModuleUser); ?>;
+        const tabBrancheUser = <?php echo json_encode($tabBrancheUser); ?>;
     </script>
 
 
