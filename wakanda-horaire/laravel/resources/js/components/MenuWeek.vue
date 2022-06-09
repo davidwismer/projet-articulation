525dd42aac5bc5c99a8a0c001f15fe0d255b8a86
David Wismer <david.wismer@heig-vd.ch>
2022-06-09 10:16:03 +0200

    Gerer controller avec user_id

---
+++ wakanda-horaire/laravel/resources/js/components/MenuWeek.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+const toggle = ref(true);
+</script>
+
+<template>
+    <div>
+        <button @click="toggle = !toggle">
+            <img v-if="toggle" class="viewMoreImg" src="./assets/vectorMenuViewMore.svg" />
+            <img v-if="!toggle" class="viewMoreImg" src="./assets/vectorMenuViewLess.svg" />
+        </button>
+    </div>
+</template>
+
+<style>
+</style>
